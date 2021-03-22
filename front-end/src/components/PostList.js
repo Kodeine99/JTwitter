daa0201ba9e507a6dc1ca922faee58854bdd79a4
Kodeine99 <tung.nv2510@gmail.com>
2021-03-22 20:57:53 +0700

    use axios, proxy settings

--- front-end/src/components/PostList.js
+++ front-end/src/components/PostList.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import "../assets/css/Post.css";
 import PostItem from "./PostItem";
+import axios from "axios";
 
 function PostList() {
+  const getAllPosts = useCallback(async () => {
+    try {
+      const option = {
+        method: "get",
+        url: "/api/v1/posts",
+      };
+      // Axios tra lai response chua posts
+      const response = await axios(option);
+      const posts = response.data.data.posts;
+      console.log(posts);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+  useEffect(() => {
+    getAllPosts();
+  }, [getAllPosts]);
   return (
     <>
       <section className="post-section">
