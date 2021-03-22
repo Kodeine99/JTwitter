0822ffdab0df7b5ff901d4a9d6f08718130f5bed
Kodeine99 <tung.nv2510@gmail.com>
2021-03-22 20:43:41 +0700

    AppReducer settings and AppContext settings

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,29 +3,36 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./components/Main";
+import AppReducer from "./reducers/AppReducer";
+import { useReducer } from "react";
+import AppContext from "./components/AppContext";
 
 function App() {
+  const initState = { user: null, post: [] };
+  const [state, dispatch] = useReducer(AppReducer, initState);
   return (
     <Router>
-      <div className="App">
-        <div className="container">
-          <Header />
-          <Switch>
-            <Route exact path="/login">
-              <Login />
-            </Route>
-            <Route exact path="/register">
-              <Register />
-            </Route>
-            <Route exact path="/">
-              <Main />
-            </Route>
-            <Route exact path="*">
-              <div>Page not found</div>
-            </Route>
-          </Switch>
+      <AppContext.Provider values={{ state, dispatch }}>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Switch>
+              <Route exact path="/login">
+                <Login />
+              </Route>
+              <Route exact path="/register">
+                <Register />
+              </Route>
+              <Route exact path="/">
+                <Main />
+              </Route>
+              <Route exact path="*">
+                <div>Page not found</div>
+              </Route>
+            </Switch>
+          </div>
         </div>
-      </div>
+      </AppContext.Provider>
     </Router>
   );
 }
