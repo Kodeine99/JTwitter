f113bf998e602336f6e38c95b7c72c5c368af1fc
Kodeine99 <tung.nv2510@gmail.com>
2021-04-02 16:59:04 +0700

    Handle create one post

--- front-end/src/components/Form.js
+++ front-end/src/components/Form.js
@@ -1,17 +1,60 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import "../assets/css/Form.css";
+import axios from "axios";
+import AppContext from "./AppContext";
 
 function Form() {
+  const { state, dispatch } = useContext(AppContext);
+  const { user } = state;
+  const [postInput, setPostInput] = useState({ content: "" });
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const onSubmitHandle = async (e) => {
+    try {
+      e.preventDefault();
+      const token = localStorage.getItem("token");
+      const option = {
+        method: "post",
+        url: "/api/v1/posts",
+        data: postInput,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await axios(option);
+      const { post } = response.data.data;
+      const author = { _id: post.author, name: user.userName };
+      dispatch({
+        type: "CREATE_ONE_POST",
+        payload: {
+          ...post,
+          author,
+          isEditable: true,
+        },
+      });
+      // Reset post
+      setPostInput({ content: "" });
+    } catch (error) {
+      setErrorMessage(error.response.data.message);
+    }
+  };
   return (
     <div>
       <section className="form-section">
-        <form className="form">
+        <form className="form" onSubmit={onSubmitHandle}>
+          {errorMessage && (
+            <div className="error-message">Error: {errorMessage}</div>
+          )}
           <textarea
             name="content"
             id="content"
             type="text"
             className="content"
             placeholder="What's on your mind?"
+            value={postInput.content}
+            onChange={(e) => {
+              setPostInput({ ...postInput, [e.target.name]: e.target.value });
+            }}
           ></textarea>
           <button className="btn" type="submit">
             Tweet
