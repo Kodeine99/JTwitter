35c40e6abce60c04e8b640bcfed29bbf85e63c42
Kodeine99 <tung.nv2510@gmail.com>
2021-04-02 16:36:36 +0700

    Handle update/delete one post

--- front-end/src/components/PostItem.js
+++ front-end/src/components/PostItem.js
@@ -1,9 +1,49 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import axios from "axios";
+import AppContext from "./AppContext";
 
 function PostItem({ post }) {
+  const { dispatch } = useContext(AppContext);
+  const [postToEdit, setPostToEdit] = useState(post);
   const [openEditForm, setOpenEditForm] = useState(false);
   const [openDeleteConfirm, setOpenDeleteConfirm] = useState(false);
   let date = new Date(post.createdAt);
+  // console.log(post);
+  const updatePost = async () => {
+    try {
+      setOpenEditForm(false);
+      // Lay token tu localStorage, vi them sua xoa deu can token
+      const token = localStorage.getItem("token");
+      const option = {
+        method: "put",
+        url: `/api/v1/posts/${post._id}`,
+        data: postToEdit,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      await axios(option);
+      dispatch({ type: "UPDATE_ONE_POST", payload: { ...postToEdit } });
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+  const deletePost = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const option = {
+        method: "delete",
+        url: `/api/v1/posts/${post._id}`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      await axios(option);
+      dispatch({ type: "DELETE_ONE_POST", payload: { _id: post._id } });
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
   return (
     <>
       <div className="post-item">
@@ -21,7 +61,7 @@ function PostItem({ post }) {
               {openDeleteConfirm ? (
                 <>
                   <span className="delete_question">Are you sure ?</span>
-                  <span>Yes</span>
+                  <span onClick={() => deletePost()}>Yes</span>
                   <span onClick={() => setOpenDeleteConfirm(false)}>No</span>
                 </>
               ) : (
@@ -42,11 +82,19 @@ function PostItem({ post }) {
                 id="content"
                 className="content"
                 placeholder="What's ?"
+                value={postToEdit.content}
+                onChange={(e) =>
+                  setPostToEdit({ ...postToEdit, content: e.target.value })
+                }
               >
                 {post.content}
               </textarea>
               <div className="btn-container">
-                <button className="btn" type="button">
+                <button
+                  className="btn"
+                  type="button"
+                  onClick={() => updatePost()}
+                >
                   Update
                 </button>
                 <button
